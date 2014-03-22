730e1f4295ce093b8849555c1fb57228e35e5984
Josep <josep_a11@hotmail.com>
2014-03-22 20:48:15 +0100

    c

--- controller/employee_month.js
+++ controller/employee_month.js
@@ -7,35 +7,38 @@ var User_Vote = require('../model/user_vote'),
 exports.get = function(req, res){
 
     User_Vote.count({}, function(err, c)
-        {
-            console.log('Numero de vots TOTAAAAAAALS = ' + c);
-            if ( c == 0 ) return res.json({ok: false, err: "There is no employee of the month yet. No one has voted."});
+    {
+        console.log('Numero de vots TOTAAAAAAALS = ' + c);
+        if ( c == 0 ) return res.json({ok: false, err: "There is no employee of the month yet. No one has voted."});
+        else
+        { //No podem fer operacions d'agregacio si no hi ha cap entrada a la bbdd
+            User_Vote
+                .aggregate(
+                { $group: {
+                    _id: { user_votat: '$user_votat' , user_votador: '$user_votador'} //Agrupem per eliminar duplicats
+                }},
+                { $group: {
+                    _id: '$_id.user_votat', count: { $sum: 1 } //Agrupem per la id de l'user_votat i incrementem cada cop
+                }},
+                //{ $sort : { count : 1 }}, //va be
+                { $project: { _id:0, user_votat: '$_id', count: 1} }, //Canviem el nom id per user votat i mostrem el count
+                { $sort: { count: -1}} //Ordenem amb ordre descendent de manera que a la casella 0 hi haurà el més votat
+            )
+                .exec( function(err, result)
+                {
+                    if (err){ res.json({ok : false, err: err})}
+                    if (!result)
+                    {
+                        return res.json( {ok:false , err:"No s'ha trobat correctament l'empleat del mes"});
+                        //return false;
+                    }
+                    req.params.id = result[0].user_votat;
+                    user.getOne(req,res); //Retornara les dades de l'employee of the month
+                });
+        }
+    });
 
-        });
 
-    User_Vote
-        .aggregate(
-        { $group: {
-            _id: { user_votat: '$user_votat' , user_votador: '$user_votador'} //Agrupem per eliminar duplicats
-        }},
-        { $group: {
-            _id: '$_id.user_votat', count: { $sum: 1 } //Agrupem per la id de l'user_votat i incrementem cada cop
-        }},
-        //{ $sort : { count : 1 }}, //va be
-        { $project: { _id:0, user_votat: '$_id', count: 1} }, //Canviem el nom id per user votat i mostrem el count
-        { $sort: { count: -1}} //Ordenem amb ordre descendent de manera que a la casella 0 hi haurà el més votat
-    )
-        .exec( function(err, result)
-        {
-            if (err){ res.json({ok : false, err: err})}
-            if (!result)
-            {
-                return res.json( {ok:false , err:"No s'ha trobat correctament l'empleat del mes"});
-               //return false;
-            }
-            req.params.id = result[0].user_votat;
-            user.getOne(req,res); //Retornara les dades de l'employee of the month
-        });
 }
 
 /*
